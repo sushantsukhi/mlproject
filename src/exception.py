42a9a2c26f5b1ade574a9e33a92fae91e79d1948
sushantsukhi <sushantsukhi@gmail.com>
2024-11-06 18:33:38 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -22,3 +22,13 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+
+        a = 1 / 0
+    except Exception as e:
+
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)
--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,8 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
@@ -14,3 +15,7 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+
+if __name__ == "__main__":
+    logging.info("Logging has started...")
